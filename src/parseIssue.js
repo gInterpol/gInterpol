3ef524daf645d8155548b61c206c3abbb698994f
Sardonyx <sardonyx@sardonyx.me>
2021-01-06 11:01:07 +0300

    added missing import

--- src/parseIssue.js
+++ src/parseIssue.js
@@ -1,5 +1,5 @@
 const steamIDRegex = /<!-- SteamID64'ü alt satıra yaz -->(\n|\r\n)(.*?)(\n|\r\n)<!-- SteamID64'ü üst satıra yaz -->/gm
-
+const fetch = require("node-fetch")
 
 /**
  * @param {string} issue
@@ -9,7 +9,7 @@ module.exports = async function(issue) {
      const query = steamIDRegex.exec(issue)
      if (!query) return
 
-     const steamID64 = query[1]
+     const steamID64 = query[2]
 
      const res = await fetch(`https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key=${process.env.APIKEY}&format=json&steamids=${steamID64}`).then(res => res.json())
 
